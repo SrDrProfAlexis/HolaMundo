5814b5eb101df02975e03438a12b5eb044343bbd
Administrator <Administrator@Administrador.mshome.net>
2019-02-20 14:31:03 -0500

    comit y push

--- pruebas1/src/aplicationController/AppInicioController.java
+++ pruebas1/src/aplicationController/AppInicioController.java
@@ -1,4 +1,4 @@
-package application;
+package aplicationController;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
---
+++ pruebas1/src/aplicationEntities/Detalle.java
@@ -0,0 +1,5 @@
+package aplicationEntities;
+
+public class Detalle {
+
+}
