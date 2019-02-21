7c2e18f89c27423dee5ae62595100d2b3ac0365c
usuario <usuario@usuario-PC>
2019-02-21 01:29:52 -0500

    objetivo logrado

--- pruebas1/src/aplicationController/ClienteController.java
+++ pruebas1/src/aplicationController/ClienteController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class ClienteController {
@@ -23,12 +25,27 @@ public class ClienteController {
 
     @FXML
     private TextField txt_telefono;
-
+    
+    ClienteController clco;
+    
     @FXML
     void aceptar() 
     {
-    	
-    	
+    	try {
+    		Stage stg = new Stage();
+        	FXMLLoader loader = new FXMLLoader();
+        	AnchorPane root = (AnchorPane)loader.load(getClass().getResource("../aplicationFxml/Ventana.fxml").openStream());
+        	VentanaController vcc = (VentanaController)loader.getController();
+        	vcc.recibirCliente(clco, txt_nombre.getText(), txt_direccion.getText(), txt_cedula.getText(), txt_email.getText(), txt_telefono.getText());
+        	Scene scene = new Scene(root);
+        	stg.setScene(scene);
+        	stg.alwaysOnTopProperty();
+        	stg.initModality(Modality.APPLICATION_MODAL);
+        	stg.show();
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
     	
     }
     
--- pruebas1/src/aplicationController/VentanaController.java
+++ pruebas1/src/aplicationController/VentanaController.java
@@ -3,8 +3,35 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 public class VentanaController {
 
-    @FXML
+	@FXML
     private Label lbl_NombreCliente;
+
+    @FXML
+    private Label lbl_cedula;
+
+    @FXML
+    private Label lbl_email;
+
+    @FXML
+    private Label lbl_direccion;
+
+    @FXML
+    private Label lbl_telefono;
+    
+    
+    ClienteController cc;
+    
+    public void recibirCliente(ClienteController ccc, String nombre, String dir, String ced, String email, String tel) {
+    	lbl_NombreCliente.setText(nombre);
+    	lbl_cedula.setText(ced);
+    	lbl_direccion.setText(dir);
+    	lbl_email.setText(email);
+    	lbl_telefono.setText(tel);    	
+    	cc=ccc;
+    }
+    
+    
+    
     
     
     @FXML
